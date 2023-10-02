5429c64222cb46cb8d4e18b29d15c79fb6c77433
Minh Doan <minhadoan@gmail.com>
2023-10-02 17:31:39 -0500

    change src on video

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -35,7 +35,7 @@ export default function () {
 
         <section className="storeTour">
           <video autoPlay loop muted>
-            <source src="../assets/video.mp4" type="video/mp4" />
+            <source src="/src/assets/video.mp4" type="video/mp4" />
           </video>
           <div className="textContent">
             <h3>Rift Toronto Flagship</h3>
