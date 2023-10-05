72371a6b504ee588bfa71880f450669e8869e176
Minh Doan <minhadoan@gmail.com>
2023-10-05 16:13:06 -0500

    pls god

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 // Styles
 import "./App.css";
 // Hooks, components
-import { useEffect, useLayoutEffect, useState } from "react";
+// import { useEffect, useLayoutEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import axios from "axios";
+// import axios from "axios";
 
 // Custom Components
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import UploadItem from "./pages/UploadItem";
-import Inventory from "./pages/Inventory";
+// import UploadItem from "./pages/UploadItem";
+// import Inventory from "./pages/Inventory";
 import Admin from "./pages/Admin";
 import Edit from "./components/edit";
+import Create from "./components/create";
+import RecordList from "./components/recordList";
 
 export default function App() {
   return (
@@ -25,8 +27,8 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/edit/:id" element={<Edit />} />
-            <Route path="/upload" element={<UploadItem />} />
-            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/recordList" element={<RecordList />} />
           </Routes>
           <Footer />
         </HelmetProvider>
